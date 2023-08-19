10452ee744b79000feb306019f3be520d612d487
AlanHawk323 <alanhawk323@gmail.com>
2023-08-18 19:37:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,110 +1,16 @@
-// === DO NOT EDIT THIS REGION ===
-// Read the comments to understand how the program is structured.
-
-// Prompt the user for a list of integers separated by commas.
-const userInputString = prompt(
-  "Please enter some integers separated by commas.",
-  "1,2,3,4,5"
-);
-
-// Split the string of numbers into an array of strings.
-const stringArray = userInputString.split(",");
-
-// Convert the array of strings into an array of numbers.
-const numbers = [];
-for (let i = 0; i < stringArray.length; i++) {
-  const str = stringArray[i];
-  const number = parseInt(str);
-  numbers.push(number);
-}
-
-// Peform some calculations on the numbers.
-console.log(numbers);
-console.log(`You have given ${getLength(numbers)} numbers.`);
-console.log(`The sum of your numbers is ${getSum(numbers)}.`);
-console.log(`The mean of your numbers is ${getMean(numbers)}.`);
-console.log(`The smallest of your numbers is ${getMin(numbers)}.`);
-console.log(`The largest of your numbers is ${getMax(numbers)}.`);
-console.log(`The range of your numbers is ${getRange(numbers)}.`);
-console.log(`The even numbers you gave are ${getEvens(numbers)}.`);
-console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
-
-// === EDIT THE CODE BELOW ===
-// Complete the functions below to make the program work!
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the length of the array
- */
-function getLength(numbers) {return stringArray.length}
-console.log(getLength (numbers))
-  // TODO
-
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the sum of the numbers
- */
-function getSum(numbers)
-{let result = 0;
-for(let i = 0; i < numbers.length; i++) {result += numbers[i]}
-return result}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the mean of the numbers
- */
-function getMean(numbers)
-{let result = getSum(numbers) / 5
-return result}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the smallest of the numbers
- */
-function getMin(numbers)
-{let smallest = Infinity
-  for(let i = 0; i < numbers.length; i++)
-  if(numbers[i] < smallest)
-  {smallest = numbers[i]}
-return smallest}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the largest of the numbers
- */
-function getMax(numbers)
-{{let biggest = 0
-  for(let i = 0; i < numbers.length; i++)
-  if(numbers[i] > biggest)
-  {biggest = numbers[i]}
-return biggest}}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the range of the numbers (max - min)
- */
-function getRange(numbers)
-{let result = getMax(numbers) - getMin(numbers)
-  return result}
-  //biggest number - smallest number
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number[]} the even numbers in the array
- */
-function getEvens(numbers)
-{let result = numbers
-for(let i = 0; i < numbers.length; i++)
-{if(numbers[i]%2 === 0)
-return result}}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number[]} the odd numbers in the array
- */
-function getOdds(numbers)
-{let result = []
-    for(let i = 0; i < numbers.length; i++)
-    {if(numbers[i]%2 !== 0)
-    return result}}
+//console.log("testing testing")
+//six default flavours for testing: vanilla,vanilla,vanilla,strawberry,coffee,coffee
+alert("Give us up to six of these Froyo flavours.")
+//one normal alert because of how much text there was
+const userFlavours = prompt ("Separated only by commas please:",
+"vanilla,vanilla,vanilla,strawberry,coffee,coffee");
+//will set up an array to split the flavours when I get back
+const array = userFlavours.split ( "," );
+const stats = {};
+for(let i = 0; i < array.length; i++)
+{const flavours = array[i];
+if(stats[flavours] === undefined)
+{stats[flavours] =0;}
+stats[flavours] = stats[flavours] +1;}
+//Activate the loop with a console log
+console.log(stats);
